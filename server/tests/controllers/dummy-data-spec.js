f495a7630d234d2aeac7cfa60cf5c0c19b311893
Sanni Michael Tomiwa <sannimichaelse@gmail>
2018-11-09 04:50:13 +0100

    ch(tests): Write unit tests

--- server/tests/controllers/dummy-data-spec.js
+++ server/tests/controllers/dummy-data-spec.js
@@ -6,4 +6,159 @@ chai.use(chaiHttp);
 chai.should();
 
 describe('Dummy Data Controller Test', () => {
+  /*
+   * Test the /GET route
+   */
+  describe('/GET REQUEST', () => {
+    it('it should GET all parcels', (done) => {
+      chai
+        .request(server)
+        .get('/api/v1/parcels')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have
+            .property('responseMessage')
+            .to.equals('Successfully fetched all parcel parcels');
+          res.body.should.have.property('responseCode').to.equals('00');
+          res.body.should.have.property('data').to.be.an('array');
+          done();
+        });
+    });
+
+    it('it should GET parcel by parcelid specified', (done) => {
+      chai
+        .request(server)
+        .get('/api/v1/parcels/2222')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have
+            .property('responseMessage')
+            .to.equals('Parcel found');
+          res.body.should.have.property('responseCode').to.equals('00');
+          res.body.should.have.property('data').to.be.an('object');
+          done();
+        });
+    });
+
+    it('it should not GET parcel id is not found', (done) => {
+      const parcelid = 7;
+      chai
+        .request(server)
+        .get(`/api/v1/parcels/${parcelid}`)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.deep.equal({
+            responseCode: '01',
+            responseMessage: 'Parcel not found'
+          });
+          done();
+        });
+    });
+  });
+
+  /*
+* Test the /POST route
+*/
+  describe('/POST REQUEST', () => {
+    it('it should make a post request if all fields are not empty ', (done) => {
+      chai
+        .request(server)
+        .post('/api/v1/parcels/')
+        .send({
+          uuid: 'mk9',
+          username: 'sullivan',
+          userId: '5',
+          parcelId: '443215',
+          parcelWeigth: '122kg',
+          parcelName: 'Tecno Camon C9',
+          parcelDestination: 'Onitsha',
+          parcelLocation: 'Lagos',
+          parcelStatus: 'progress'
+        })
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.deep.equal({
+            responseCode: '00',
+            responseMessage: 'New parcel created successfully'
+          });
+          done();
+        });
+    });
+
+    it('it should not make a post request if some fields are empty ', (done) => {
+      const body = {
+        uuid: '',
+        username: 'sullivan',
+        userId: '5',
+        parcelId: '443215',
+        parcelWeigth: '122kg',
+        parcelName: 'Tecno Camon C9',
+        parcelDestination: 'Onitsha',
+        parcelLocation: 'Lagos',
+        parcelStatus: 'progress'
+      };
+
+      chai
+        .request(server)
+        .post('/api/v1/parcels/')
+        .send(body)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.deep.equal({
+            responseCode: '01',
+            responseMessage:"\"uuid\" is not allowed to be empty"
+          });
+          done();
+        });
+    });
+
+    it('it should throw an error when you try to add duplicate data ', (done) => {
+      const data = {
+        uuid: 'mk9',
+        username: 'sullivan',
+        userId: '5',
+        parcelId: '443215',
+        parcelWeigth: '122kg',
+        parcelName: 'Tecno Camon C9',
+        parcelDestination: 'Onitsha',
+        parcelLocation: 'Lagos',
+        parcelStatus: 'progress'
+      };
+
+      chai
+        .request(server)
+        .post('/api/v1/parcels/')
+        .send(data)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.deep.equal({
+            responseCode: '01',
+            responseMessage:
+              'Parcel Already Exists. Make sure uuid and parcelId are unique'
+          });
+          done();
+        });
+    });
+  });
+
+  /*
+* Test the /UPDATE route
+*/
+  describe('/CANCEL REQUEST', () => {
+    it('it should cancel request by parcel id', (done) => {
+      const parcelid = 2222;
+      chai
+        .request(server)
+        .get(`/api/v1/parcels/${parcelid}/cancel`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have
+            .property('responseMessage')
+            .to.equals('Parcel Order Cancelled');
+          res.body.should.have.property('responseCode').to.equals('00');
+          res.body.should.have.property('data').to.be.an('object');
+          done();
+        });
+    });
+  });
 });
